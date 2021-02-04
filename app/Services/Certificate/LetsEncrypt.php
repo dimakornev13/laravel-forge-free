e356cec6c947253a63def0a2e5631e0c8a2f638b
mustang92 <dimakornev13@yandex.ru>
2021-02-04 08:02:27 +0300

    - forced type hint from null (shell_exec) to string

--- app/Services/Certificate/LetsEncrypt.php
+++ app/Services/Certificate/LetsEncrypt.php
@@ -3,8 +3,6 @@
 namespace App\Services\Certificate;
 
 
-use App\Events\CertificateObtained;
-use App\Models\LogEvent;
 use App\Models\Site;
 
 class LetsEncrypt extends CertificateObtain
@@ -12,39 +10,33 @@ class LetsEncrypt extends CertificateObtain
 
     public function obtainCertificate(Site $site)
     {
-        $url = $site->getUrl();
-
-        $this->result = shell_exec("sudo certbot --nginx --noninteractive --agree-tos --register-unsafely-without-email  -d {$url} -d www.{$url}");
+        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive --agree-tos --register-unsafely-without-email  -d {$site->getUrl()} -d www.{$site->getUrl()}");
 
         if (stripos($this->result, 'Congratulations! You have successfully enabled') === false)
             throw new \Error("Couldn't take certificate for {$site->getUrl()} \n {$this->result}");
 
-        event(new CertificateObtained());
+//        event(new CertificateObtained());
     }
 
 
     public function renewCertificate(Site $site)
     {
-        $url = $site->getUrl();
-
-        $this->result = shell_exec("sudo certbot --nginx --noninteractive -d {$url}");
+        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive -d {$site->getUrl()}");
 
 //        if (stripos($this->result, 'Congratulations! You have successfully enabled') === false)
 //            throw new \Error("Couldn't take certificate for {$site->getUrl()} \n {$this->result}");
 
-        event(new CertificateObtained());
+//        event(new CertificateObtained());
 
     }
 
     public function deleteCertificate(Site $site){
-        $url = $site->getUrl();
-
-        $this->result = shell_exec("sudo certbot delete --noninteractive --cert-name {$url}");
+        $this->result = (string)shell_exec("sudo certbot delete --noninteractive --cert-name {$site->getUrl()}");
 
         if (stripos($this->result, 'Deleted all files relating to certificate') === false)
             throw new \Error("Couldn't delete certificate for {$site->getUrl()} \n {$this->result}");
 
-        event(new CertificateObtained());
+//        event(new CertificateObtained());
 
     }
 }
