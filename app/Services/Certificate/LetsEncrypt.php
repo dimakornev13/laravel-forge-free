ad5fe22aa28a2697bceed9bb4b270e347e631acb
mustang92 <dimakornev13@yandex.ru>
2021-02-04 08:05:58 +0300

    - forgot to add redirect for shell command

--- app/Services/Certificate/LetsEncrypt.php
+++ app/Services/Certificate/LetsEncrypt.php
@@ -10,33 +10,22 @@ class LetsEncrypt extends CertificateObtain
 
     public function obtainCertificate(Site $site)
     {
-        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive --agree-tos --register-unsafely-without-email  -d {$site->getUrl()} -d www.{$site->getUrl()}");
+        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive --agree-tos --register-unsafely-without-email  -d {$site->getUrl()} -d www.{$site->getUrl()} 2>&1");
 
         if (stripos($this->result, 'Congratulations! You have successfully enabled') === false)
             throw new \Error("Couldn't take certificate for {$site->getUrl()} \n {$this->result}");
-
-//        event(new CertificateObtained());
     }
 
 
     public function renewCertificate(Site $site)
     {
-        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive -d {$site->getUrl()}");
-
-//        if (stripos($this->result, 'Congratulations! You have successfully enabled') === false)
-//            throw new \Error("Couldn't take certificate for {$site->getUrl()} \n {$this->result}");
-
-//        event(new CertificateObtained());
-
+        $this->result = (string)shell_exec("sudo certbot --nginx --noninteractive -d {$site->getUrl()} 2>&1");
     }
 
     public function deleteCertificate(Site $site){
-        $this->result = (string)shell_exec("sudo certbot delete --noninteractive --cert-name {$site->getUrl()}");
+        $this->result = (string)shell_exec("sudo certbot delete --noninteractive --cert-name {$site->getUrl()} 2>&1");
 
         if (stripos($this->result, 'Deleted all files relating to certificate') === false)
             throw new \Error("Couldn't delete certificate for {$site->getUrl()} \n {$this->result}");
-
-//        event(new CertificateObtained());
-
     }
 }
