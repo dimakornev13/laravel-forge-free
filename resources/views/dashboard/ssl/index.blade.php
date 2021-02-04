f82eae0beb6fbd351f94f85a8e72be2759639f71
mustang92 <dimakornev13@yandex.ru>
2021-02-04 07:53:47 +0300

    - fix link (ssl page) to form

--- resources/views/dashboard/ssl/index.blade.php
+++ resources/views/dashboard/ssl/index.blade.php
@@ -5,7 +5,11 @@
         <div class="w-6/12 m-auto">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 flex flex-col space-y-12">
-                    <a href="{{ url(route('ssl.obtain', ['site' => $site])) }}" class="btn rounded-md bg-green-600 hover:bg-green-700 text-white p-3">Obtain certificate</a>
+                    <form action="{{ route('ssl.obtain', ['site' => $site]) }}" method="post">
+                        @csrf
+                        @method('put')
+                        <button type="submit" class="btn rounded-md bg-green-600 hover:bg-green-700 text-white p-3">Obtain certificate</button>
+                    </form>
                 </div>
             </div>
         </div>
