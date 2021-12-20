3e150db7dd6029f1324fe5ea2ea0b6425e7a8307
mustang92 <dimakornev13@yandex.ru>
2021-12-20 19:58:58 +0300

    - script subsequence has been changed

--- resources/views/dashboard/deploy/script.blade.php
+++ resources/views/dashboard/deploy/script.blade.php
@@ -4,13 +4,13 @@
 ROOT_PATH=/home/{{ getHostUser() }}/{{ $site->getUrl() }}
 DEPLOY_DIR=$(date "+%d.%m.%y-%H:%M.%s")
 
-git clone {{ $site->getRepository() }} --single-branch $ROOT_PATH/$DEPLOY_DIR 2>&1
+git clone {{ $site->getRepository() }} --single-branch $ROOT_PATH/$DEPLOY_DIR
 
 export COMPOSER_HOME="$HOME/.config/composer";
-COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --prefer-dist --working-dir=$ROOT_PATH/$DEPLOY_DIR 2>&1
-COMPOSER_MEMORY_LIMIT=-1 composer dump-autoload --no-interaction --optimize-autoloader --no-dev --working-dir=$ROOT_PATH/$DEPLOY_DIR 2>&1
+COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --prefer-dist --working-dir=$ROOT_PATH/$DEPLOY_DIR
+COMPOSER_MEMORY_LIMIT=-1 composer dump-autoload --no-interaction --optimize --working-dir=$ROOT_PATH/$DEPLOY_DIR
 
-yarn --cwd $ROOT_PATH/$DEPLOY_DIR install 2>&1 && yarn --cwd $ROOT_PATH/$DEPLOY_DIR run production 2>&1
+yarn --cwd $ROOT_PATH/$DEPLOY_DIR install && yarn --cwd $ROOT_PATH/$DEPLOY_DIR run production
 
 # handle storage directory
 if [ ! -d $ROOT_PATH/storage ]; then
@@ -22,12 +22,13 @@ ln -sfn $ROOT_PATH/storage $ROOT_PATH/$DEPLOY_DIR
 
 ln -sfn $ROOT_PATH/.env $ROOT_PATH/$DEPLOY_DIR
 
-sudo service php8.0-fpm reload 2>&1
-
-# todo opcache enable
-php $ROOT_PATH/$DEPLOY_DIR/artisan migrate --force 2>&1
-php $ROOT_PATH/$DEPLOY_DIR/artisan optimize 2>&1
-php $ROOT_PATH/$DEPLOY_DIR/artisan storage:link 2>&1
-
 rm -rf $ROOT_PATH/www
 ln -sfn -T $ROOT_PATH/$DEPLOY_DIR $ROOT_PATH/www
+
+sudo service php8.0-fpm reload
+
+# todo opcache enable
+php $ROOT_PATH/$DEPLOY_DIR/artisan migrate --force
+php $ROOT_PATH/$DEPLOY_DIR/artisan optimize
+php $ROOT_PATH/$DEPLOY_DIR/artisan storage:link
+php $ROOT_PATH/$DEPLOY_DIR/artisan queue:restart
